792111f501cd14453f7842e7feb03f8631395c55
Kirill <121458409+Oknehcvotil@users.noreply.github.com>
2024-02-08 21:14:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://ralogisticks.vercel.app", // Установите корректный домен вашего фронтенда
+    origin: "https://ralogisticks.vercel.app",
     credentials: true,
   })
 );
